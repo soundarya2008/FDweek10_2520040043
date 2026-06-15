de8c0d896642ad1e8f41c1c5a2d68eafb35b2b8e
soundarya valli <soundaryavalli2013@gmail.com>
2026-06-15 15:38:49 +0530

    Create ProctectedRoute.jsx

---
+++ ProctectedRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ isLoggedIn, children }) {
+
+  if (!isLoggedIn) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
+
